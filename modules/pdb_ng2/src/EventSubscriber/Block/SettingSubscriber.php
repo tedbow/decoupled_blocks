33b226ff2dbaf092010abccd70b2bb3ef86915fc
Emmanuel Cortes <emmanuel.cortes@nybblegroup.com>
2016-04-05 02:05:19 -0300

    Added event subscribers to subscribe pdb block events in the pdb_ng2 sub-module

---
+++ modules/pdb_ng2/src/EventSubscriber/Block/SettingSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\pdb_ng2\EventSubscriber\Block\SettingSubscriber.
+ */
+
+namespace Drupal\pdb_ng2\EventSubscriber\Block;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+use Drupal\pdb\Event\Block\PdbBlockEvents;
+use Drupal\pdb\Event\Block\SettingEvent;
+
+/**
+ * Gets block settings for the ng2 presentation.
+ */
+class SettingSubscriber implements EventSubscriberInterface {
+
+  public function onBlockSetting(SettingEvent $event) {
+    $component = $event->getComponent();
+    $settings  = $event->getSettings();
+
+    $info = $component['info'];
+    if ($info['presentation'] == 'ng2') {
+      if (!isset($settings['apps'])) {
+        $settings['apps'] = array();
+      }
+      $settings['apps'][$info['machine_name']] = array(
+        'uri' => '/' . $info['path']
+      );
+    }
+
+    $event->setSettings($settings);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[PdbBlockEvents::SETTING][] = array('onBlockSetting');
+
+    return $events;
+  }
+
+}
